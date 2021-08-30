fdc54b3eb7838de19f38a07d849a5003279bd9d9
Benjamin Capps <cappsb@southwestern.edu>
2021-08-30 08:03:05 -0500

    Created basic Monsters array
    Stores miscellaneous information about monsters.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,27 +1,32 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      string: 'Hello Bennie'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1',
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2',
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3',
+        },
+      ]
 
     }
   }
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-
-          <button onClick= {() => this.setState({ string: 'Hello Johnny' })}>
-          Change Text
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
+        }
       </div>
     );
   }
